384a17e615042a15f2ea3d3d3ba6d7dacc07023f
vcsawant <viren.c.sawant@gmail.com>
2018-08-25 20:39:13 -0500

    update game mechanics

--- src/server/bughouse/routes/game.py
+++ src/server/bughouse/routes/game.py
@@ -6,53 +6,67 @@ import logging
 
 LOGGER = logging.getLogger(__name__)
 
-board_a = chess.Board()
-board_b = chess.Board()
 
-placeable_white_a = []
-placeable_white_b = []
-placeable_black_a = []
-placeable_black_b = []
+class Board:
+    def __init__(self, identifier):
+        self.board = chess.Board()
+        self.identifier = identifier
+        self.placeable_white = [0, 0, 0, 0, 0]
+        self.placeable_black = [0, 0, 0, 0, 0]
+
+    def validate_move(self, move):
+        # TODO: complete definition
+        return True
+
+    def move(self, move):
+        self.board.push(move)
 
 
-def dict_to_move(dict):
-    from_square = dict.get('from_square', None)
-    to_square = dict.get('to_square', None)
-    promotion = dict.get('promotion', None)
-    drop = dict.get('drop', None)
+board_a = Board("A")
+board_b = Board("B")
 
-    move = Move(from_square=int(from_square), to_square=int(to_square),
+
+def parse_move(dict):
+    from_square = dict.get('from_square')
+    from_square = int(from_square) if from_square is not None else None
+    to_square = dict.get('to_square')
+    to_square = int(to_square) if to_square is not None else None
+    drop = dict.get('drop')
+    drop = int(drop) if drop is not None else None
+    promotion = dict.get('promotion')
+
+    move = Move(from_square=from_square, to_square=to_square,
                 promotion=promotion, drop=drop)
     return move
 
 
-@app.route("/board/move-a/", methods=["POST"])
+def push_move_and_validate(move, board):
+    move = parse_move(request.form.to_dict())
+    if move == Move.null():
+        return Response("Null Move", status=400)
+
+    if not board.validate_move(move):
+        return Response("Invalid Move", status=400)
+
+    board.move(move)
+    LOGGER.info("pushed move on board " + board.identifier + move.uci())
+
+    return Response(status=204)
+
+
+@app.route("/game/move-a/", methods=["POST"])
 def move_a():
-    move = dict_to_move(request.form.to_dict())
-    if move != Move.null():
-        board_a.push(move)
-        LOGGER.info("pushed move")
-        LOGGER.debug("move on board A: " + move.uci())
-
-        return Response(status=204)
-
-    return Response("Move unable to be parsed", status=400)
+    move = parse_move(request.form.to_dict())
+    return push_move_and_validate(move, board_a)
 
 
-@app.route("/board/move-b/", methods=["POST"])
+@app.route("/game/move-b/", methods=["POST"])
 def move_b():
-    move = dict_to_move(request.form.to_dict())
-    if move != Move.null():
-        board_b.push(move)
-        LOGGER.info("pushed move")
-        LOGGER.debug("move on board B: " + move.uci())
-
-        return Response(status=204)
-
-    return Response("Move unable to be parsed", status=400)
+    move = parse_move(request.form.to_dict())
+    return push_move_and_validate(move, board_b)
 
 
-@app.route("/board/view")
+@app.route("/game/view")
 def view():
     LOGGER.info("viewing board")
-    return board_a.fen() + " and " + board_b.fen()
+    return board_a.board.fen() + " and " + board_b.board.fen()
